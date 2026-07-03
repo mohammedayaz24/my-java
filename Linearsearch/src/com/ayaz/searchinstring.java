6f7be8ce3cb2b3eb9727dcf49c1d4d2472fb008c
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-03 10:44:27 +0530

    search in string

--- Linearsearch/src/com/ayaz/searchinstring.java
+++ Linearsearch/src/com/ayaz/searchinstring.java
@@ -3,17 +3,40 @@ package com.ayaz;
 public class searchinstring {
     public static void main(String[] args){
         String name = "ayaz";
-        String target = "z";
-        System.out.println(search(name,target));
+        char target = 'z';
+        System.out.println("search:"+search(name,target));
+        System.out.println("Output using for each loop");
+        System.out.println(search2("search2:"+ name,target));
+
     }
 
-    static boolean search(String str, String target){
-        if (str.length() != 0) {
+    static boolean search(String str, char target){
+        if (str.length() == 0) {
             return false;
         }
 
-        for(char ch:str.toCharArray()){
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == target){
+                return true;
+            }
+        }
+        return false;
+    }
 
+    //type 2 using for each method
+    static boolean search2(String str, char target){
+        if(str.length() == 0){
+            return false;
         }
 
+        for(char ch : str.toCharArray()){
+            if(ch == target){
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
+
