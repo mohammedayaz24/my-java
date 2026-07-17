6437158ebd1a6227514f133af59d4b1c0b8acdd5
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-17 15:20:21 +0530

    nember guess

--- BinarySearch/src/com/ayaz/Binary.java
+++ BinarySearch/src/com/ayaz/Binary.java
@@ -1,7 +1,6 @@
 package com.ayaz;
 
-public class
-Binary {
+public class Binary {
     public static void main(String[] args) {
         int[] arr = {2,4,6,9,11,12,14,20,36,48};
         int target = 36;
---
+++ BinarySearch/src/com/ayaz/NumGuess.java
@@ -0,0 +1,27 @@
+package com.ayaz;
+
+public class NumGuess {
+    public static void main(String[] args) {
+        int n = 10;
+        int mn=111;
+        int ans = number(n,mn);
+        System.out.println(ans);
+
+    }
+
+    public static int number(int n,int mn){
+        int start = 0;
+        int end = n;
+
+        while(start <= end){
+            //find the middle element
+            int mid = (start + (end - start) / 2);
+
+            if(mn < mid){
+                end = mid-1;
+            }else if (mn > mid){
+                start = mid+1;
+        }
+        return -1;
+    }
+}
