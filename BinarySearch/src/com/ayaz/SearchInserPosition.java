13e5d2954407d103fba2389b77cc7073df1e05cf
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-22 23:18:54 +0530

    Search insert position

---
+++ BinarySearch/src/com/ayaz/SearchInserPosition.java
@@ -0,0 +1,33 @@
+package com.ayaz;
+
+public class SearchInserPosition {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 7;
+        System.out.println(searchInsert(nums,target));
+
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end) {
+            //find the middle element
+            int mid = (start + (end - start) / 2);
+
+            if (target == nums[mid]) {
+                return mid;
+            } else if(target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+
+    }
+
+}
