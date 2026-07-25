51fe05c90deb7c450c218536ee188ff0ddbdf87c
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-25 23:39:50 +0530

    SingleNon duplicate value leetcode link

--- BinarySearch/src/com/ayaz/SingleNonDuplicate.java
+++ BinarySearch/src/com/ayaz/SingleNonDuplicate.java
@@ -1,5 +1,6 @@
 package com.ayaz;
 
+//https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/2081097670/
 public class SingleNonDuplicate {
     public static void main(String[] args) {
         int[] nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
