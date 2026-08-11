f5d3ebb034fd6b2c07b6169542a5ce2bcaa5de88
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-08-11 11:42:46 +0530

    largest number in an array

--- visAI/src/reverse.java
+++ visAI/src/reverse.java
@@ -1,5 +1,4 @@
 public class reverse {
-
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30, 40};
         reverse(numbers);
@@ -8,18 +7,13 @@ public class reverse {
             System.out.println(num);
         }
     }
-
     public static void reverse(int[] numbers) {
-
         int left = 0;
         int right = numbers.length - 1;
-
         while (left < right) {
-
             int temp = numbers[left];
             numbers[left] = numbers[right];
             numbers[right] = temp;
-
             left++;
             right--;
         }
