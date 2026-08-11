a3e81628a72351510fff469cf6b9d5e10c96db0a
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-08-11 13:57:21 +0530

    fining missing number in a sequence of number

---
+++ visAI/src/missingnum.java
@@ -0,0 +1,18 @@
+public class missingnum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 6};
+        int missingnum = missingnumber(arr);
+        System.out.println(missingnum);
+    }
+
+    public static int missingnumber(int[] arr){
+        long n = arr.length + 1;
+
+        int sum = 0;
+        for(int i = 0; i <= arr.length-1;i++){
+            sum += arr[i];
+        }
+        long expsum = n * (n + 1)/2;
+        return (int)(expsum-sum);
+    }
+}
