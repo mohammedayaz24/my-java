545dae46c29df443b3f839c59c8687b278fb7822
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-08-11 10:54:37 +0530

    reverse an array of number

---
+++ visAI/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+
+        for (int i = numbers.length-1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
---
+++ visAI/src/reverse.java
@@ -0,0 +1,27 @@
+public class reverse {
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+        reverse(numbers);
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+
+    public static void reverse(int[] numbers) {
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+
+            int temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
